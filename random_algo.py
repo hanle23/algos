651595cf171385943d1e5ccbef039098eea61e42
Han Le <hanle.cs23@gmail.com>
2025-02-13 01:28:29 -0500

    Change the orientation of shuffle card for better reading

--- random_algo.py
+++ random_algo.py
@@ -1,4 +1,6 @@
 import random
+
+
 def shuffle_fisher(items):
     for i in range(len(items) - 1, -1, -1):
         currRandom = random.randint(0, len(items) - 1)
@@ -7,17 +9,19 @@ def shuffle_fisher(items):
         items[i], items[currRandom] = items[currRandom], items[i]
     return items
 
+
 def shuffle_card(items):
     for _ in range(15):
         start = random.randint(0, int(len(items) // 4))
         end = random.randint(int(len(items) // 2), int(len(items) // 1.33))
-        mid_stack = items[start:end]
         top = items[0: start]
+        mid_stack = items[start:end]
         bottom = items[end:]
         top.extend(bottom)
         top.extend(mid_stack)
         items = top
     return items
+
 
 def main():
     pass
